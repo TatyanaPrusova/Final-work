30cd176575f2060f56dcbf989178fd5820e303a3
Tatyana <t666ax89@yandex.ru>
2022-12-15 22:59:04 +0300

    writing code

--- Task/Program.cs
+++ Task/Program.cs
@@ -4,3 +4,27 @@
 // [“1234”, “1567”, “-2”, “computer science”] → [“-2”]
 // [“Russia”, “Denmark”, “Kazan”] → []
 
+string[] array1 = new string[] { "1234", "Hello", "yes", "no", };
+string[] array2 = new string[array1.Length];
+
+int count = 0;
+for (int i = 0; i < array1.Length; i++)
+{
+    if (array1[i].Length <= 3)
+    {
+        array2[count] = array1[i];
+        count++;
+    }
+}
+
+void PrintArray(string[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+    Console.WriteLine();
+}
+
+PrintArray(array1);
+PrintArray(array2);
